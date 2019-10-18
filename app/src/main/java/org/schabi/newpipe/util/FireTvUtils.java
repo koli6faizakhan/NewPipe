0ab293bb61616224ff1fa42738cd9402373912c0
koli6faizakhan <56574412+koli6faizakhan@users.noreply.github.com>
2019-10-18 10:29:31 +0530

    Update FireTvUtils.java

--- app/src/main/java/org/schabi/newpipe/util/FireTvUtils.java
+++ app/src/main/java/org/schabi/newpipe/util/FireTvUtils.java
@@ -1,10 +1,19 @@
 package org.schabi.newpipe.util;
 
-import org.schabi.newpipe.App;
+ import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
 
-public class FireTvUtils {
+ import org.schabi.newpipe.App;
+
+ public class FireTvUtils {
+    @SuppressLint("InlinedApi")
     public static boolean isFireTv(){
         final String AMAZON_FEATURE_FIRE_TV = "amazon.hardware.fire_tv";
-        return App.getApp().getPackageManager().hasSystemFeature(AMAZON_FEATURE_FIRE_TV);
+        
+
+         PackageManager pm =  App.getApp().getPackageManager();
+
+         return pm.hasSystemFeature(AMAZON_FEATURE_FIRE_TV)
+                || pm.hasSystemFeature(PackageManager.FEATURE_LEANBACK);
     }
 }
